0b5058332f688d3741e26968b6ce0025d768af37
Abhijit Chandrakant Pawar <abhi.c.pawar@gmail.com>
2015-01-22 18:09:48 -0500

    Homework 1 completed
    Still lot of scope for optimization

--- misc/cis194/hw1/creditcard.hs
+++ misc/cis194/hw1/creditcard.hs
@@ -21,20 +21,41 @@ doubleEveryOther :: [Integer] -> [Integer]
 doubleEveryOther [] = []
 doubleEveryOther (x:y:xs) = [x] ++ [2*y] ++ doubleEveryOther xs
 
+
 --sum all the digits of the list.
 sumDigits :: [Integer] -> Integer
-sumDigits = undefined
+sumDigits  = sum.(concat.(map toDigits))
 
+--validate the credit card number
+validate :: Integer -> Bool
+validate val 
+	| val `mod` 10 ==0 = True
+	| otherwise = False
+	 
 
 main :: IO ()
 main = do
 	putStrLn "Validating the credit card number"
 	putStrLn "Enter your credit card number"
 	card <- getLine
+
+	--putStrLn "converting the number to the list of digits"
 	let digits = toDigits (read card :: Integer)
-	putStrLn $ show digits
+	--putStrLn $ show digits
+
+	--putStrLn "reversing the number to the list of digits"
 
 	let revDigits = toDigitsRev (read card :: Integer)
-	putStrLn $ show revDigits
-	let doubleDigits = doubleEveryOther [1,2,3,4]
-	putStrLn $ show doubleDigits
+	--putStrLn $ show revDigits
+
+	--putStrLn "doubling every other number of the reversed list"
+	let doubleDigits = doubleEveryOther revDigits
+	--putStrLn $ show doubleDigits
+
+	--putStrLn "suming each digit of this reversed list"
+	let sumofVal = sumDigits doubleDigits
+	--putStrLn $ show $ sumofVal
+
+	putStrLn "validating the number"
+	let ret = validate sumofVal
+	putStrLn $ show ret
