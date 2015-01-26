9f84c4f3eba853ca1a2238012c909bac14d0c997
Abhijit Chandrakant Pawar <abhi.c.pawar@gmail.com>
2015-01-26 17:46:46 -0500

    LogAnalysis updates to support the main

--- misc/cis194/hw2/LogAnalysis.hs
+++ misc/cis194/hw2/LogAnalysis.hs
@@ -4,11 +4,28 @@ module LogAnalysis where
 
 import Log
 
+parseError :: [String] -> LogMessage
+parseError (error:value:timestamp:xs) = LogMessage (Error (read value::Int)) (read timestamp::Int) (unwords xs) 
+
+parseInfo :: [String] -> LogMessage
+parseInfo (info:timestamp:xs) = LogMessage Info (read timestamp::Int) (unwords xs)
+
+
+parseWarning :: [String] -> LogMessage
+parseWarning (warning:timestamp:xs) = LogMessage Warning (read timestamp::Int) (unwords xs)
+
+
+parseWrapper :: [String] -> LogMessage
+parseWrapper log = case (head log) of
+			"I" -> parseInfo log
+			"W" -> parseWarning log
+			"E" -> parseError log
+--			_  ->  Unknown head.words log
 
 --define a function to parse the individual line from the log file
 
 parseMessage :: String -> LogMessage
-parseMessage = undefined
+parseMessage log = (parseWrapper.words) log
 
 
 {-- parse the entire log file at once and return the contents as list of the 
@@ -16,8 +33,4 @@ parseMessage = undefined
 --}
 
 parse :: String -> [LogMessage]
-parse = undefined
-
-
-main :: IO ()
-main = putStrLn "Hello Log"
+parse log =  map parseMessage $ lines log
