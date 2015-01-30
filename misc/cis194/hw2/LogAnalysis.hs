88f0ad366e3e057070586765c0627e93b463db7c
Abhijit Chandrakant Pawar <abhi.c.pawar@gmail.com>
2015-01-30 14:58:20 -0500

    Exercise 2 and 3 : Initial commits

--- misc/cis194/hw2/LogAnalysis.hs
+++ misc/cis194/hw2/LogAnalysis.hs
@@ -35,8 +35,37 @@ parseMessage log = (parseWrapper.words) log
 parse :: String -> [LogMessage]
 parse log =  map parseMessage $ lines log
 
+-- define a function to get the timestamp of the LogMessage
+
+getLogMsgTimeStamp :: LogMessage -> Int
+getLogMsgTimeStamp ( LogMessage m t d ) = t
+
+--define a function to get the timestamp of the current node of the message tree
+
+getMsgTreeTimeStamp :: MessageTree -> Int
+getMsgTreeTimeStamp (Node l t r) =  getLogMsgTimeStamp t
+
+--data MessageTree = Leaf | Node MessageTree LogMessage MessageTree
+
+
 --define a function which will insert the LogMessage in the binary tree where
 -- the timestamp on left tree of the node is less and greater on the right tree.
 
 insert :: LogMessage -> MessageTree -> MessageTree
-insert = undefined
+insert logMsg Leaf = Node Leaf logMsg Leaf
+insert logMsgInput (Node leftTree treeLog rightTree)  
+						| inTimeStamp == msgTimeStamp = Node leftTree logMsgInput rightTree
+						| inTimeStamp < msgTimeStamp = Node (insert logMsgInput leftTree) treeLog rightTree
+						| inTimeStamp > msgTimeStamp = Node leftTree treeLog (insert logMsgInput rightTree)
+						where
+							inTimeStamp = getLogMsgTimeStamp logMsgInput
+							msgTimeStamp = getMsgTreeTimeStamp (Node leftTree treeLog rightTree)
+
+
+--build the messageTree
+build :: [LogMessage] -> MessageTree
+--build [] = Node (Leaf (Unknown "unknowns") Leaf)
+build = foldr insert Leaf 
+
+
+
