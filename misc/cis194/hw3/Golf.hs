6093c813a779b45c81793149ebc73e27fc5f8e0f
Abhijit Chandrakant Pawar <abhi.c.pawar@gmail.com>
2015-02-03 09:02:34 -0500

    Homework 3 : Initial skeleton

---
+++ misc/cis194/hw3/Golf.hs
@@ -0,0 +1,16 @@
+module Golf where
+
+--Excercise 1 Hopscotch
+skips :: [a] -> [[a]]
+skips = undefined
+
+
+--Excercise 2 Local Maxima
+localMaxima :: [Integer] -> [Integer]
+localMaxima = undefined
+
+--Excercise 3 Histogram
+histogram :: [Integer] -> String
+histogram = undefined
+
+
