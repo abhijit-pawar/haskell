1e22c5c373c2032213bea0f7528ec9b1ed9d0c83
Abhijit Chandrakant Pawar <abhi.c.pawar@gmail.com>
2015-02-06 11:55:34 -0500

    Final Hopscotch
    The thing nagging me is about the string index

--- misc/cis194/hw3/Golf.hs
+++ misc/cis194/hw3/Golf.hs
@@ -1,14 +1,15 @@
 module Golf where
 
-myTake :: [String] -> Int -> Char 
-myTake str int = (\y x -> y!!0!!x ) str int
+myTake :: String -> Int -> Char 
+myTake str int = (\y x -> y!!x ) str int
 
-mine :: [String] -> Int-> String
-mine str int = (\i->(\ls z-> map((\x -> myTake x) ls ) $ (\listLength -> take listLength. filter ((\y x -> x `mod` y == 0) z) $ [1..listLength - 1 ] ).length $ ls !! 0) i) str int
+mine :: String -> Int-> String
+mine str int = (\i->(\ls z-> map((\x -> myTake x) ls) $ (\listLength -> take listLength. filter ((\y x -> x `mod` y == 0) z) $ [1..listLength -1] ).length $ ls) i) str int
 
 --Excercise 1 Hopscotch
---skips :: [String] -> [[String]]
-skips ls = (\l-> map (mine ls) [1..l]) $ length $ ls!!0 
+skips :: String -> [String]
+skips ls = (\l-> map (mine ("_"++ls)) [1..l+1]) $ length $ ls
+
 
 --Excercise 2 Local Maxima
 localMaxima :: [Integer] -> [Integer]
