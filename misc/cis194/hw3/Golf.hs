6ddc65686e3fe6cf1f02e66bb537a4da028a6c49
Abhijit Chandrakant Pawar <abhi.c.pawar@gmail.com>
2015-02-06 11:37:39 -0500

    Hopscotch done
    But it needs to have extra char at the beginning of the string to work correctly

--- misc/cis194/hw3/Golf.hs
+++ misc/cis194/hw3/Golf.hs
@@ -1,9 +1,14 @@
 module Golf where
 
---Excercise 1 Hopscotch
-skips :: [a] -> [[a]]
-skips = undefined
+myTake :: [String] -> Int -> Char 
+myTake str int = (\y x -> y!!0!!x ) str int
 
+mine :: [String] -> Int-> String
+mine str int = (\i->(\ls z-> map((\x -> myTake x) ls ) $ (\listLength -> take listLength. filter ((\y x -> x `mod` y == 0) z) $ [1..listLength - 1 ] ).length $ ls !! 0) i) str int
+
+--Excercise 1 Hopscotch
+--skips :: [String] -> [[String]]
+skips ls = (\l-> map (mine ls) [1..l]) $ length $ ls!!0 
 
 --Excercise 2 Local Maxima
 localMaxima :: [Integer] -> [Integer]
