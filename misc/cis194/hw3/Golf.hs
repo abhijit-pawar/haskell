a082bd9101e0830d18628734e384ca3669dc1b06
Abhijit Chandrakant Pawar <abhi.c.pawar@gmail.com>
2015-02-09 07:55:15 -0500

    A clean solution instead of the mess I did earlier. I forgot that zipping will give exactly what I am looking at
    Just zip the contents of the list with the numbers and find the multiple of them from the list. Take the first element of the tuple

--- misc/cis194/hw3/Golf.hs
+++ misc/cis194/hw3/Golf.hs
@@ -1,14 +1,14 @@
 module Golf where
 
-myTake :: String -> Int -> Char 
-myTake str int = (\y x -> y!!x ) str int
+--Take the items from the list whose position is multiple of the given number
+multiplesOf :: Int -> [a] -> [a]
+multiplesOf _ [] = []
+multiplesOf n xs = map fst $ filter (\(a,b) -> b `mod` n == 0) $ zip xs [1..]
 
-mine :: String -> Int-> String
-mine str int = (\i->(\ls z-> map((\x -> myTake x) ls) $ (\listLength -> take listLength. filter ((\y x -> x `mod` y == 0) z) $ [1..listLength -1] ).length $ ls) i) str int
+--Excercise 1  Hopscotch
+skips :: [a] -> [[a]]
+skips xs = map (\l -> multiplesOf l xs) [1..length xs]
 
---Excercise 1 Hopscotch
-skips :: String -> [String]
-skips ls = (\l-> map (mine ("_"++ls)) [1..l+1]) $ length $ ls
 
 
 --Excercise 2 Local Maxima
