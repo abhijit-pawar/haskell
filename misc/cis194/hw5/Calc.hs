0e7c6a73c7ee14389b830d600dd6cce9883f427f
Abhijit Chandrakant Pawar <abhi.c.pawar@gmail.com>
2015-02-12 11:59:00 -0500

    Excercise 02

--- misc/cis194/hw5/Calc.hs
+++ misc/cis194/hw5/Calc.hs
@@ -1,6 +1,7 @@
 module Calc where
 
 import ExprT as ET
+import Parser
 
 --Excercise 01
 
@@ -8,3 +9,16 @@ eval :: ExprT -> Integer
 eval (Lit x) = x
 eval (ET.Add x y) = (eval x) + (eval y)
 eval (ET.Mul x y) = (eval x) *( eval y)
+
+--Excercise 02
+evalStr :: String -> Maybe Integer
+evalStr p = case parseExp Lit ET.Add ET.Mul p of
+		Nothing -> Nothing
+		Just e  -> Just (eval e)
+ 
+evalStrFunctor :: String -> Maybe Integer
+evalStrFunctor = fmap eval . parseExp Lit ET.Add ET.Mul
+			
+--evalStr Nothing = Nothing
+--evalStr Just ( ExprT t) = eval t
+
