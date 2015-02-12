471ba48fa9e41f5305efa4afa328382ae54c331c
Abhijit Chandrakant Pawar <abhi.c.pawar@gmail.com>
2015-02-12 15:57:05 -0500

    Excercise 04

--- misc/cis194/hw5/Calc.hs
+++ misc/cis194/hw5/Calc.hs
@@ -32,12 +32,41 @@ instance Expr ExprT where
 	add = Add
 	mul = Mul
 
-instance Expr Integer where
-	lit = id
-	add = (+)
-	mul = (*)
-
 reify :: ExprT -> ExprT
 reify = id
 
+--Excercise 04
 
+instance Expr Integer where
+        lit = id
+        add = (+)
+        mul = (*)
+
+instance Expr Bool where
+	lit a  
+		| a > 0 = True
+		| a <=0 = False
+	add = (||)
+	mul = (&&)
+
+newtype MinMax = MinMax Integer deriving (Eq,Show)
+
+instance Expr MinMax where
+	lit = MinMax
+	add (MinMax x) (MinMax y) = MinMax (max x y)
+	mul (MinMax x) (MinMax y) = MinMax (min x y)
+
+newtype Mod7 = Mod7 Integer deriving (Show,Eq)
+
+instance Expr Mod7 where
+	lit x = Mod7 ( x `mod` 7)
+	add (Mod7 x) (Mod7 y) = Mod7 ( (x + y) `mod` 7)
+	mul (Mod7 x) (Mod7 y) = Mod7 ( (x*y) `mod` 7)
+
+testExp :: Expr a => Maybe a
+testExp = parseExp lit add mul "(3* -4) + 5"
+
+testInteger = testExp :: Maybe Integer
+testBool = testExp :: Maybe Bool
+testMM = testExp :: Maybe MinMax
+testSat = testExp :: Maybe Mod7
