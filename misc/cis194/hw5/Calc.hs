998014f8b681ebe80b9cd31eb7157fdb337ece80
Abhijit Chandrakant Pawar <abhi.c.pawar@gmail.com>
2015-02-12 11:47:10 -0500

    simple expression evaluator

--- misc/cis194/hw5/Calc.hs
+++ misc/cis194/hw5/Calc.hs
@@ -1,9 +1,10 @@
-import ExprT
+module Calc where
+
+import ExprT as ET
 
 --Excercise 01
 
 eval :: ExprT -> Integer
-eval = undefined
-
-
-
+eval (Lit x) = x
+eval (ET.Add x y) = (eval x) + (eval y)
+eval (ET.Mul x y) = (eval x) *( eval y)
