13def8add9ef78602b9203834c8427b4192afe69
Abhijit Chandrakant Pawar <abhi.c.pawar@gmail.com>
2015-02-17 17:27:15 -0500

    Excercise 01 : A JoinList for Monoidal annotations

--- misc/cis194/hw7/JoinList.hs
+++ misc/cis194/hw7/JoinList.hs
@@ -1,4 +1,25 @@
 module JoinList where
 
-data JoinList m a = Empty | Single m a | Append m (JoinList m a) (JoinList m a)
+import Data.Monoid
+
+data JoinList m a = Empty 
+			| Single m a 
+			| Append m (JoinList m a) (JoinList m a)
+		deriving (Eq, Show)
+
+--Excerccise 01
+{-- Write an append function for JoinLists that yields a new JoinList whose monoidal
+	annotation is derived from those of the two arguments
+--}
+(+++) :: Monoid m => JoinList m a -> JoinList m a -> JoinList m a
+jl1 +++ jl2  = Append (tag jl1 <> tag jl2 ) jl1 jl2
+
+--Helper function to get the root of the JoinList. 
+--As tag is a function working on typeclass for Monoid of JoinList, we have to implement all the data constructors for JoinList for a Monoid
+
+tag :: Monoid m => JoinList m a -> m
+tag Empty 		= mempty
+tag (Single m a) 	= m
+tag (Append m _ _ ) 	= m
+
 
