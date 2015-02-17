148e79ab29a7be34f7751c2f0ac89b60ae425828
Abhijit Chandrakant Pawar <abhi.c.pawar@gmail.com>
2015-02-17 18:14:12 -0500

    indexJ and dropJ

--- misc/cis194/hw7/JoinList.hs
+++ misc/cis194/hw7/JoinList.hs
@@ -1,6 +1,11 @@
+{-# LANGUAGE FlexibleInstances, TypeSynonymInstances #-}
+
 module JoinList where
 
 import Data.Monoid
+import Sized
+import Editor
+import Buffer
 
 data JoinList m a = Empty 
 			| Single m a 
@@ -23,3 +28,26 @@ tag (Single m a) 	= m
 tag (Append m _ _ ) 	= m
 
 
+--Excercise 02
+--1. Implment the indexing
+
+--get the size of the JoinList
+sizeOf :: (Sized m , Monoid m) => JoinList m a -> Int
+sizeOf = getSize.size.tag
+
+indexJ :: (Sized b , Monoid b) => Int -> JoinList b a -> Maybe a
+indexJ _ Empty = Nothing
+indexJ i _ | i < 0 = Nothing
+indexJ i jl | i > sizeOf jl = Nothing
+indexJ i (Single _ a) = Just a
+indexJ i (Append m l r ) | i < sizeOf l = indexJ i l
+			 | otherwise = indexJ (i - sizeOf l) r
+
+dropJ :: (Sized b, Monoid b) => Int -> JoinList b a -> JoinList b a 
+dropJ _ Empty = Empty
+dropJ i a | i <= 0 = a
+dropJ i (Single m a) | i == 1 = Empty
+dropJ i jl | i > sizeOf jl = Empty
+dropJ i (Append m l r ) 
+	| i < sizeOf l = (dropJ i l) +++ r
+	| otherwise = dropJ ( i - sizeOf l) r
