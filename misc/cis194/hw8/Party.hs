71afc9f472e9f28862f914cd910ebca5dca45eba
Abhijit Chandrakant Pawar <abhi.c.pawar@gmail.com>
2015-02-19 11:51:49 -0500

    Monoidal instance of GuestList along with maximum fun having GuestList

--- misc/cis194/hw8/Party.hs
+++ misc/cis194/hw8/Party.hs
@@ -11,3 +11,13 @@ import Data.List
 -- Excercise add an Employee to a GuestList
 glCons :: Employee -> GuestList -> GuestList
 glCons emp (GL empss funs) = GL (emp:empss) (funs + empFun emp)
+
+--Add Monoidal instance of the GuestList
+
+instance Monoid GuestList where
+	mempty = GL [] 0
+	(GL es fun ) `mappend` (GL es' fun') = GL (es ++ es') (fun + fun')
+
+--GuestList supports comparator on fun 	quotient as its instance of order
+moreFun :: GuestList -> GuestList -> GuestList
+moreFun = max
