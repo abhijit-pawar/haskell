4736b722556a7290acdcb4f88044dfc6bb8de425
Abhijit Chandrakant Pawar <abhi.c.pawar@gmail.com>
2015-02-18 23:14:36 -0500

    Excercise 1

--- misc/cis194/hw8/Party.hs
+++ misc/cis194/hw8/Party.hs
@@ -1,1 +1,13 @@
+{-# OPTIONS_GHC -fno-warn-orphans #-}
 
+module Party where
+
+import Employee
+import Data.Monoid
+import Data.Tree
+import Data.List
+
+
+-- Excercise add an Employee to a GuestList
+glCons :: Employee -> GuestList -> GuestList
+glCons emp (GL empss funs) = GL (emp:empss) (funs + empFun emp)
